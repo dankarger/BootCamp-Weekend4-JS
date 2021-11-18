7432ddb791b66a8db47df078f57e7b2ee36d295d
dankarger <karger.dan@gmail.com>
2021-11-18 13:59:48 +0200

    second commit

---
+++ 6_1-Mumbling.js
@@ -0,0 +1,14 @@
+function accum(str) {
+    let arrStr = str.toLowerCase().split('');
+    let result =[];
+    arrStr.forEach((letter,index)=>{
+        let extra= letter.repeat(index);
+        let firstLetter= letter.toUpperCase();
+        result.push(firstLetter+extra);
+    })
+    return result.join('-')
+}
+
+//Test:
+console.log(accum("abcd"));
+console.log(accum("RqaEzty"));
